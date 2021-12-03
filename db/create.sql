cd15098b7456e81df78406a283fe979b35450beb
Rohanlachman <113342@emmauscollege.nl>
2021-12-03 09:29:36 +0000

    lettertype

--- db/create.sql
+++ db/create.sql
@@ -21,19 +21,19 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('nette schoenen', 'Dit is een exclusief paar schoenen gedragen door Hugo de Jonge, deze schoen is gepersonaliseerd en er is 
+insert into products (name, description, code, price) values ('Nette schoenen', 'Dit is een exclusief paar schoenen gedragen door Hugo de Jonge, deze schoen is gepersonaliseerd en er is 
 maar 1 exemplaar van. Hugo is overigens niet inbegrepen bij dit product.', '816905633-0', 999);
 
-insert into products (name, description, code, price) values ('klompen', 'Puur oudhollandse klompen gemaakt van hout', '077030122-3', 11);
+insert into products (name, description, code, price) values ('Klompen', 'Puur oudhollandse klompen gemaakt van hout', '077030122-3', 11);
 
-insert into products (name, description, code, price) values ('cars crocs', 'Dit zijn exclusieve Cars crocs. Met deze crocs ren je twee keer zo snel. ktjauw!.', '445924201-X', 13.5);
+insert into products (name, description, code, price) values ('Cars crocs', 'Dit zijn exclusieve Cars crocs. Met deze crocs ren je twee keer zo snel. ktjauw!.', '445924201-X', 13.5);
 
 
 insert into products (name, description, code, price) values ('Jordan 1 High J-Balvin', 'Dit zijn de welbekende Jordan 1s, deze schoenen zijn ontworpen door de bekende zanger J-Balvin. Er zijn hier maar 4 exemplaren van.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('gouden schoenen', 'Nu al een klassieker, gedragen door de bekende worstelaar John Cena', '686928463-6', 14);
-insert into products (name, description, code, price) values ('zeeman schoenen', 'Dit zijn exclusieve schoenen van de Zeeman. Met deze schoenen ga je in zee man', '492662523-7', 14);
+insert into products (name, description, code, price) values ('Gouden schoenen', 'Nu al een klassieker, gedragen door de bekende worstelaar John Cena', '686928463-6', 14);
+insert into products (name, description, code, price) values ('Zeeman schoenen', 'Dit zijn exclusieve schoenen van de Zeeman. Met deze schoenen ga je in zee man', '492662523-7', 14);
 
 insert into products (name, description, code, price) values ('Balenciaga schoen', 'Deze schoen is speciaal voor mensen die iets tegen sokken hebben.', '492662524-7', 14);
 
-insert into products (name, description, code, price) values ('coc(clash of clans) schoenen', 'Met deze schoen ben je de bom. Net als die ene die uit de ballon valt.', '492662525-7', 14);
+insert into products (name, description, code, price) values ('Coc(clash of clans) schoenen', 'Met deze schoen ben je de bom. Net als die ene die uit de ballon valt.', '492662525-7', 14);
 
